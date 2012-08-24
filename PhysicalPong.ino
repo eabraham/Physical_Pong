bc3465564a290f27ff18cb076c093d4956456d65
Eric Abrahamsen <ericthredsbeard@gmail.com>
2012-08-24 16:12:39 -0400

    Just basic functions to start thought process.

---
+++ PhysicalPong.ino
@@ -0,0 +1,46 @@
+void setup(){
+  
+}
+
+void loop(){
+  
+}
+
+void zeroMotors(){
+
+}
+
+void centerBall(){
+  
+}
+
+void resetScore(){
+
+}
+void setBallTarget(){
+  
+}
+void moveMotors(){
+ 
+}
+void paddleInterrupt(){
+  
+}
+boolean atBallTarget(){
+  
+}
+
+boolean isGoalScored(){
+  
+}
+
+void releaseBall(){
+}
+
+void incrementScore(){
+}
+boolean isResetButtonPushed(){
+  
+}
+
+
